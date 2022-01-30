c38f71a1b6df47fcc2e8f3c740fe00ae32ef8a75
Alex Waller <alex@artofalexwaller.com>
2022-01-29 23:38:36 -0500

    kingOfTheHill

--- players.js
+++ players.js
@@ -49,27 +49,91 @@ function displayPlayers() {
     const nameHeader = document.createElement("h1");
     nameHeader.innerHTML = playerX.name;
     nameHeader.class = "name-header";
-    playerLi.append(nameHeader);
+    playersUl.append(nameHeader);
     // create cityHeader
     const cityHeader = document.createElement("h2");
     cityHeader.class = "city-header";
     cityHeader.innerHTML = playerX.city;
-    playerLi.append(cityHeader);
+    playersUl.append(cityHeader);
     // create playerImage
     const playerImage = document.createElement("img");
     playerImage.class = "player-image";
     playerImage.src = playerX.img;
     playerImage.height = 400;
     playerImage.width = 300;
-    playerLi.append(playerImage);
-    // create edit button
-
+    playersUl.append(playerImage);
     // create break
     const playerBreak = document.createElement("br");
     playerBreak.class = "player-break";
-    playerLi.append(playerBreak);
+    playersUl.append(playerBreak);
+    // create another break
+    const playerBreak2 = document.createElement("br");
+    playerBreak2.class = "player-break-2";
+    playersUl.append(playerBreak2);
+    // create edit button
+    const editBtn = document.createElement("button");
+    editBtn.className = "edit-btn";
 
-    playersUl.append(playerLi);
+    editBtn.innerHTML = "edit player info";
+    playersUl.append(editBtn);
+    // create delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+    deleteBtn.innerHTML = "delete-player-info";
+    playersUl.append(deleteBtn);
+    // create modify button
+    const modifyBtn = document.createElement("button");
+    modifyBtn.className = "modify-btn";
+    modifyBtn.innerHTML = "modify player info";
+
+    //...................................................
+    //...................................................
+    deleteBtn.addEventListener("click", function () {
+      let target = playerX.id;
+      players.splice(target - 1, 1);
+      for (let i = 0; i < players.length; i++) {
+        if (players[i].id > target) {
+          players[i].id = players[i].id - 1;
+        }
+      }
+      let data5 = JSON.stringify(players);
+      localStorage.setItem("players-list", data5);
+      count = players.length + 1;
+      let newCount = JSON.stringify(count);
+      localStorage.setItem("persist-count", newCount);
+      playersUl.removeChild(nameHeader);
+      playersUl.removeChild(cityHeader);
+      playersUl.removeChild(playerImage);
+      playersUl.removeChild(playerBreak);
+      playersUl.removeChild(playerBreak2);
+      playersUl.removeChild(editBtn);
+      playersUl.removeChild(deleteBtn);
+    });
+    //....................................................
+    editBtn.addEventListener("click", function () {
+      nameInput.value = playerX.name;
+      cityInput.value = playerX.city;
+      imageInput.value = playerX.img;
+      formDiv.removeChild(submitBtn);
+      formDiv.appendChild(modifyBtn);
+    });
+    //.....................................................
+    modifyBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      playerX.name = nameInput.value;
+      nameHeader.innerHTML = playerX.name;
+      playerX.city = cityInput.value;
+      cityHeader.innerHTML = playerX.city;
+      playerX.img = imageInput.value;
+      playerImage.src = playerX.img;
+      formDiv.removeChild(modifyBtn);
+      formDiv.append(submitBtn);
+      let data15 = JSON.stringify(players);
+      localStorage.setItem("players-list", data15);
+      nameInput.value = "";
+      cityInput.value = "";
+      imageInput.value = "";
+    });
   });
 }
 
